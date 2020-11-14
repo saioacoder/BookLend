62218cb630864a24946c5e70e68d8ea6b5782604
Saioa <saioa@hodeidesign.com>
2020-11-14 12:31:24 +0100

    Arreglar indentaciones

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+
+import Book from './pages/book';
+import Collection from './pages/collection';
+import Dashboard from './pages/dashboard';
+import Home from './pages/home';
+import Landing from './pages/landing';
+import MyBooks from './pages/myBooks';
+
 function App() {
-  return (
-    <div>
-      Holaaaaa
-    </div>
-  );
+	return (
+		<>
+			<Router>
+				<Header />
+				<Switch>
+					<Route exact path="/">
+						<Landing />
+					</Route>
+					<Route exact path="/admin/inicio/">
+						<Dashboard />
+					</Route>
+					<Route exact path="/admin/coleccion/">
+						<Collection />
+					</Route>
+					<Route exact path="/inicio/">
+						<Home />
+					</Route>
+					<Route exact path="/libro/">
+						<Book />
+					</Route>
+					<Route exact path="/mis-prestamos/">
+						<MyBooks />
+					</Route>
+				</Switch>
+				<Footer />
+			</Router>
+		</>
+	);
 }
 
 export default App;
