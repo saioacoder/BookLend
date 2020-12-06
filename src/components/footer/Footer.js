944bc1fad9763b348fb2bdd8a0e52cc92869d721
Saioa <saioa@hodeidesign.com>
2020-12-06 18:58:10 +0100

    Arreglo del pie

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,21 @@
+import { Link } from 'react-router-dom';
+
 import './Footer.scss';
 
 const Footer = () => {
+
+	const actualYear = () => {
+		const now = new Date();
+		return now.getFullYear();
+	};
+
 	return (
 		<>
 			<footer className="footer">
 				<div className="container">
-					<div>Copyright 2020</div>
-					<div>Other links</div>
+					<div className="footer_copyright">© {actualYear()} BookLend</div>
+					<Link to="/">Sobre BookLend</Link>
+					<Link to="/">Contacto</Link>
 				</div>
 			</footer>
 		</>
